1c8ee7009a4c84616ced00c803387588eafff6d2
shantanu <shantanuchavhan10@gmail.com>
2023-10-11 16:25:49 +0530

    commit

---
+++ App.js
@@ -0,0 +1,19 @@
+
+function downloadResume() {
+    // Create an anchor element
+    var anchor = document.createElement('a');
+    anchor.style.display = 'none';
+
+    // Set the href attribute to the URL of your resume file
+    anchor.href = 'Resume.pdf'; // Replace 'path_to_resume.pdf' with the actual file path
+
+    // Set the download attribute to specify the default file name
+    anchor.download = 'shantanu_chavhan_resume.pdf'; // Replace with your desired file name
+
+    // Trigger a click event on the anchor to start the download
+    document.body.appendChild(anchor);
+    anchor.click();
+
+    // Clean up the anchor element
+    document.body.removeChild(anchor);
+}
