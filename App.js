309c040052a25c49aeb1791ab094b6e8535c5dd0
shantanu <shantanuchavhan10@gmail.com>
2023-10-19 17:15:28 +0530

    commi

--- App.js
+++ App.js
@@ -19,7 +19,56 @@ function downloadResume() {
 }
 
 
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contact-form');
+    const confirmationMessage = document.getElementById('confirmation-message');
+    
+    const sg = new SendGrid('YOUR_SENDGRID_API_KEY');
+
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(contactForm);
+
+        const emailData = {
+            to: 'recipient@example.com',
+            from: formData.get('email'), // Use the form email as the sender
+            subject: 'Contact Form Submission',
+            text: `Name: ${formData.get('name')}\nEmail: ${formData.get('email')}\nMessage: ${formData.get('message')}`,
+        };
+
+        sg.send(emailData)
+            .then(() => {
+                contactForm.style.display = 'none';
+                confirmationMessage.style.display = 'block';
+                contactForm.reset();
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+});
+
+
+function contactMe(){
+    // Select all elements with the class "hero-section-img"
+    const elements = document.querySelectorAll('.hero-section-img');
+
+    // Add a class to hide the elements
+    elements.forEach(element => {
+        element.classList.add('hidden');
+});
+
+}
 
 
 
-
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
